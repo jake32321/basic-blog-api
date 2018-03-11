42a3d87781c743482f2fcb4ffb36a80eabb942f5
jake32321 <jacob.reed95@siu.edu>
2018-03-11 14:59:43 -0700

    added tests for posts create

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const Joi = require('joi');
-const Boom = require('boom');
 const post = require('../services/post');
 const router = require('express').Router();
 
@@ -10,32 +8,20 @@ router.get('/', async (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    const postRes = post.createPost(req.body);
+    res.send(postRes);
+});
+
 router.get('/:id', async (req, res) => {
     await post.getPostById(req).then(result => {
         res.send(result);
     })
 });
 
-router.post('/', (req, res) => {
-    const result = Joi.validate(req.body, post.postSchema);
-
-    if(result.error === null){
-        const postRes = post.createPost(req.body);
-        res.send(postRes);
-    } else {
-        res.send(Boom.badRequest('Request not formed correctly.'));
-    }
-});
-
 router.put('/:id', async (req, res) => {
-    const result = Joi.validate(req.body, post.schemas.postSchema);
-
-    if(result.error === null){
-        const postUpdateRes = await post.updatePost(req);
-        res.send(postUpdateRes);
-    } else {
-        res.send(Boom.badRequest('Request not formed correctly for update.'))
-    }
+    const postUpdateRes = await post.updatePost(req.body);
+    res.send(postUpdateRes);
 });
 
 module.exports = router;
--- services/post.js
+++ services/post.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const Boom = require('boom')
 const firebase = require('firebase');
 const db = require('./../db');
 const shortid = require('shortid');
@@ -16,38 +17,48 @@ internals.schemas.postSchema = Joi.object().keys({
     date: Joi.date()
 });
 
-internals.createPost = function(req) {
-    const res = {
+exports.createPost = function(req) {
+    const result = Joi.validate(req, internals.schemas.postSchema)
+
+    if (result.error == null) {
+        let res = {
+            title: req.title,
+            author: req.author,
+            textBody: req.textBody,
+            date: new Date().toISOString()
+        }
+    
+        const postId = shortid.generate()
+        ref.child(postId).set(res);
+        res.id = postId;
+        return res;
+    } else {
+        throw Boom.badRequest(result.error);
+    }
+};
+
+exports.getPosts = async function(req){
+   return await ref.once('value')
+};
+
+exports.getPostById = async function(req){
+    const postByIdRef = ref.child(req.params.id);
+
+    return await postByIdRef.once('value');
+}
+
+exports.updatePost = async function(req, id){
+
+    const postByIdRef = ref.child(req.params.id);
+
+    const dataToPost = {
         title: req.title,
         author: req.author,
         textBody: req.textBody,
         date: new Date().toISOString()
     }
 
-    ref.child(shortid.generate()).set(res);
-    return res;
-};
-
-internals.getPosts = async function(req){
-   return await ref.once('value')
-};
-
-internals.getPostById = async function(req){
-    const postByIdRef = ref.child(req.params.id);
-    return await postByIdRef.once('value');
-}
-
-internals.updatePost = async function(req, id){
-    const postByIdRef = ref.child(req.params.id);
-    const dataToPost = {
-        title: req.body.title,
-        author: req.body.author,
-        textBody: req.body.textBody,
-        date: new Date().toISOString()
-    }
-
     postByIdRef.set(dataToPost)
+
     return dataToPost;
 }
-
-module.exports = internals;
