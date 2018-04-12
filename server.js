5bafe250b8701901f13429dbe299faae46933549
jake32321 <jacob.reed95@siu.edu>
2018-04-11 22:50:19 -0500

    working with hbs

--- server.js
+++ server.js
@@ -2,24 +2,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./lib/db');
+const hbs = require('express-handlebars');
 
 db.init();
 
-// Get the routes
-const postRoutes = require('./routes/post');
-const userRoutes = require('./routes/user');
 const app = express();
 
 const port = process.env.PORT || 3000;
 const ip = process.env.IP || 'localhost'
 
+app.engine('hbs', hbs({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/posts', postRoutes);
-app.use('/users', userRoutes);
+
+app.use('/posts', require('./routes/post'));
+app.use('/users', require('./routes/user'));
 
 app.get('/', (req, res) => {
-    res.send('Hello!');
+    const opts = {
+        ip: port
+    }
+
+    res.render('index', opts);
 });
 
 app.listen(port, () => {
